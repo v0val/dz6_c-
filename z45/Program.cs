a7d5387ff83b07baeb7fab874670e3b852616db8
valeryO <olennval@mail.ru>
2022-08-26 23:32:18 +0700

    number 45

---
+++ z45/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+int[] mas = new int[10];
+int[] mas2 = new int[10];
+for (int i = 0; i < 10; i++)
+{
+    mas[i] = new Random().Next(-10, 10);
+}
+for (int i = 0; i < 10; i++)
+{
+    mas2[i] = mas[i];
+}
+//mas2 = mas;
+var str = string.Join(" ", mas2);
+Console.WriteLine(str);
