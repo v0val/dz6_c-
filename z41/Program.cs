c01dbe1cc0cfb73573c1372d1aa718b58b9b4a98
valeryO <olennval@mail.ru>
2022-08-26 22:25:25 +0700

    number 41

---
+++ z41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите количество вводимых чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int Count = 0;
+double chislo = 0;
+for (int i = 1; i <= M; i++)
+{
+    chislo = Convert.ToInt32(Console.ReadLine());
+    if (chislo > 0) Count ++;
+}
+Console.Write(" количество  чисел  больше 0 = ");
+Console.WriteLine(Count);
