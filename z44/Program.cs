f2079f4c43d1f6e565e16051eebcf0dcf5b66cd3
valeryO <olennval@mail.ru>
2022-08-26 23:09:41 +0700

    number 44

---
+++ z44/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите количество чисел Фибоначи ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ряд чисел Фибоначи ");
+int x1 = 0;
+int x2 = 1;
+int summ = 0;
+Console.Write("0 1 ");
+for (int i = 1; i < n-1; i++)
+{
+    summ = x1+x2;
+    Console.Write($"{summ} ");
+    x1 = x2;
+    x2 = summ;
+}
