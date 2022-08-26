dfcf20c349f61c41dfc5c971d5c5316b0d8c8aff
valeryO <olennval@mail.ru>
2022-08-26 22:48:23 +0700

    number 43

---
+++ z43/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите целые коэффициенты 1-го уравнения");
+int k1 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите коэффициенты 2-го уравнения");
+int k2 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+if (k1 == k2 && b1 != b2) Console.WriteLine("прямые параллельны, решения нет");
+if (k1 == k2 && b1 == b2) Console.WriteLine("прямые совпали, решением является вся числовая прямая");
+if (k1 != k2) 
+{
+    double x = (b2 - b1) / (k1 - k2); 
+    double y = k1 * x + b1;
+    Console.WriteLine($"точка пересечения прямых ({x}; {y})");
+}
