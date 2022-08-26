1d2fe82fe5a42918936ab8bcb27b95889b3cf1b1
valeryO <olennval@mail.ru>
2022-08-27 00:13:37 +0700

    number dopolnitelny

---
+++ dop/Program.cs
@@ -0,0 +1,12 @@
+﻿int p = 1;
+int Proisv(int n)
+{
+    if (n == 1) return p;
+    Proisv(n-1);
+    p *= n;
+    return p;
+}
+Console.WriteLine("Введите целое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"произведение целых чисел от 1 до {n}: ");
+Console.WriteLine(Proisv(n));
